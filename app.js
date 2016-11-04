79c927ee31caaf48dd0d94eb7b9589110d7983c0
Daniel Varela <dvarela@losaltosca.gov>
2016-11-04 16:02:00 -0700

    fixed css

--- app.js
+++ app.js
@@ -3,7 +3,7 @@
 var GUIDE_BOX_API_KEY = 'rKGb7Oh50TpEuVG55ENiKRKUxCBmrYVd';
 var GUIDE_BOX_BASE_URL = 'http://api-public.guidebox.com/v1.43/us/' + GUIDE_BOX_API_KEY + '/search/movie/title/';
 var IMDB_URL = 'http://imdb.com/title/';
-function getDataFromApi(searchTerm, callback) {
+function getSearchDataFromApi(searchTerm, callback) {
     var query = GUIDE_BOX_BASE_URL + searchTerm + '/fuzzy';
     $.getJSON(query, callback)
 };
@@ -13,22 +13,22 @@ function displaySearchData(data){
   console.log(data);
   if (data.results) {
     data.results.forEach(function(item) {
-        resultElement += '<p>' +
-        "<img src=" + item.poster_240x342 + ">"+ '<p>';
+      resultElement += "<a href='#' data-featherlight='iframe'>" +
+      "<img src=" + item.poster_240x342 + "></a>";
     });
-
   } else {
     resultElement += '<p> no results <p>';
   }
   $('.js-search-results').append(resultElement)
+
 }
 
 function watchSubmit(){
   $('.js-search-form').submit(function(event){
-    $('js-search-results').empty();
+    $('.js-search-results').empty();
     event.preventDefault();
     var query = $(this).find('.js-query').val();
-    getDataFromApi(query, displaySearchData);
+    getSearchDataFromApi(query, displaySearchData);
   });
 }
 
