37f928e3947e567d0199ed2677025441ee679155
dotexe0 <dv@Daniels-MacBook-Pro.local>
2016-11-06 09:59:14 -0800

    added display on default

--- app.js
+++ app.js
@@ -3,6 +3,14 @@
 var GUIDE_BOX_API_KEY = 'rKGb7Oh50TpEuVG55ENiKRKUxCBmrYVd';
 var GUIDE_BOX_BASE_URL = 'http://api-public.guidebox.com/v1.43/us/' + GUIDE_BOX_API_KEY + '/search/movie/title/';
 var IMDB_URL = 'http://imdb.com/title/';
+
+//display popular movies by default on main page
+function defaultDisplayData(){
+  var movieSearchURL = 'http://api-public.guidebox.com/v1.43/us/' + GUIDE_BOX_API_KEY + '/movies/all/0/50/';
+  $.getJSON(movieSearchURL, displaySearchData)
+}
+
+//search any movie title
 function getSearchDataFromApi(searchTerm, callback) {
     var query = GUIDE_BOX_BASE_URL + searchTerm + '/fuzzy';
     $.getJSON(query, callback)
@@ -11,10 +19,18 @@ function getSearchDataFromApi(searchTerm, callback) {
 function displaySearchData(data){
   var resultElement = '';
   console.log(data);
+  var emptyImage = 'http://static-api.guidebox.com/misc/default_movie_240x342.jpg'
   if (data.results) {
     data.results.forEach(function(item) {
-      resultElement += "<a href='#' data-featherlight='iframe'>" +
-      "<img src=" + item.poster_240x342 + "></a>";
+      if (item.poster_240x342 != emptyImage) {
+        //get trailer links
+        var trailerLinksURL = 'https://api-public.guidebox.com/v1.43/US/' + GUIDE_BOX_API_KEY + '/movie/' + item.id + '/videos/all/0/5';
+        var links = $.getJSON(trailerLinksURL)
+        console.log(links);
+        trailerVideo = links.responseJSON;
+        resultElement += "<a href=" + trailerVideo + "data-featherlight='iframe'>" +
+        "<img src=" + item.poster_240x342 + "></a>";
+      }
     });
   } else {
     resultElement += '<p> no results <p>';
@@ -32,4 +48,7 @@ function watchSubmit(){
   });
 }
 
-$(function(){watchSubmit();});
+$(function(){
+  defaultDisplayData();
+  watchSubmit();
+});
