afc4842f88067019f3eeb357492043f899446200
dotexe0 <dv@Daniels-MacBook-Pro.local>
2016-11-07 11:38:53 -0800

    functional video, needs w/h fix

--- app.js
+++ app.js
@@ -2,11 +2,12 @@
 // var TASTE_KID_BASE_URL = 'https://www.tastekid.com/api/similar';
 var GUIDE_BOX_API_KEY = 'rKGb7Oh50TpEuVG55ENiKRKUxCBmrYVd';
 var GUIDE_BOX_BASE_URL = 'http://api-public.guidebox.com/v1.43/us/' + GUIDE_BOX_API_KEY + '/search/movie/title/';
-var IMDB_URL = 'http://imdb.com/title/';
+var quota = $.getJSON('http://api-public.guidebox.com/v1.43/us/' + GUIDE_BOX_API_KEY + '/quota');
+console.log(quota);
 
 //display popular movies by default on main page
 function defaultDisplayData(){
-  var movieSearchURL = 'http://api-public.guidebox.com/v1.43/us/' + GUIDE_BOX_API_KEY + '/movies/all/0/50/';
+  var movieSearchURL = 'http://api-public.guidebox.com/v1.43/us/' + GUIDE_BOX_API_KEY + '/movies/all/0/10/';
   $.getJSON(movieSearchURL, displaySearchData)
 }
 
@@ -17,26 +18,26 @@ function getSearchDataFromApi(searchTerm, callback) {
 };
 
 function displaySearchData(data){
-  var resultElement = '';
-  console.log(data);
-  var emptyImage = 'http://static-api.guidebox.com/misc/default_movie_240x342.jpg'
+  var emptyImage = 'http://static-api.guidebox.com/misc/default_movie_240x342.jpg';
   if (data.results) {
     data.results.forEach(function(item) {
       if (item.poster_240x342 != emptyImage) {
-        //get trailer links
-        var trailerLinksURL = 'https://api-public.guidebox.com/v1.43/US/' + GUIDE_BOX_API_KEY + '/movie/' + item.id + '/videos/all/0/5';
-        var links = $.getJSON(trailerLinksURL)
-        console.log(links);
-        trailerVideo = links.responseJSON;
-        resultElement += "<a href=" + trailerVideo + "data-featherlight='iframe'>" +
-        "<img src=" + item.poster_240x342 + "></a>";
+        //get movie details if image exists
+        var trailerLinksURL = 'https://api-public.guidebox.com/v1.43/US/' + GUIDE_BOX_API_KEY + '/movie/' + item.id;
+        $.getJSON(trailerLinksURL, function(data){
+          var resultElement = '';
+          console.log(data);
+          trailerVideo = data.trailers.web[0].embed;
+          resultElement = "<a href=" + trailerVideo + " data-featherlight='iframe'>" +
+          "<img src=" + item.poster_240x342 + "></a>";
+          console.log(resultElement);
+          $('.js-search-results').append(resultElement);
+        });
       }
     });
   } else {
     resultElement += '<p> no results <p>';
   }
-  $('.js-search-results').append(resultElement)
-
 }
 
 function watchSubmit(){
