51066c41cc1b77923814f75a09125ec7be99a5c4
dotexe0 <dv@Daniels-MacBook-Pro.local>
2016-11-11 14:13:01 -0800

    fixed styling

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-// var TASTE_KID_API_KEY = '246618-movieagg-S4Y1KTWA';
-// var TASTE_KID_BASE_URL = 'https://www.tastekid.com/api/similar';
+$(document).ready(function(){
+
 var OMDB_BASE_URL = "https://www.omdbapi.com/?i=";
 var GUIDE_BOX_API_KEY = 'rKGb7Oh50TpEuVG55ENiKRKUxCBmrYVd';
 var GUIDE_BOX_BASE_URL = 'https://api-public.guidebox.com/v1.43/us/' + GUIDE_BOX_API_KEY + '/search/movie/title/';
@@ -9,33 +9,26 @@ var quota = $.getJSON('https://api-public.guidebox.com/v1.43/us/' + GUIDE_BOX_AP
 var IMDB_URL = "https://www.imdb.com/title/";
 var ROTTEN_URL = "https://www.rottentomatoes.com/m/";
 //display popular movies by default on main page
-var start = 0;
-var end = 12;
-function defaultDisplayData(start=0, end=12){
+
+function defaultDisplayData(start=0, end=5){
   var movieSearchURL = 'https://api-public.guidebox.com/v1.43/us/' + GUIDE_BOX_API_KEY + '/movies/all/' + start +'/' + end;
   $.getJSON(movieSearchURL, displaySearchData)
 }
-
-
 
 //search any movie title
 function getSearchDataFromApi(searchTerm, callback) {
     var query = GUIDE_BOX_BASE_URL + searchTerm + '/fuzzy';
     $.getJSON(query, callback)
 };
+//grab ratings from OMDB api call
 function getDataFromOMDB(imdbLink) {
   var search = OMDB_BASE_URL + imdbLink;
-  $.getJSON(search, function(data){
-    imdb_rating = data.imdbRating;
-    meta_rating = data.Metascore;
-    var title = data.Title;
-    console.log(title);
-    console.log(meta_rating);
-    console.log(imdb_rating);
+  $.getJSON(search, function(omdb){
+    imdb_rating = omdb.imdbRating;
+    var meta_rating = omdb.Metascore;
   });
 };
-function displaySearchData(data){
-
+  function displaySearchData(data){
   var emptyImage = 'https://static-api.guidebox.com/misc/default_movie_240x342.jpg';
   if (data.results) {
     data.results.forEach(function(item) {
@@ -50,33 +43,29 @@ function displaySearchData(data){
           var genre = data.genres[0].title;
           var movieDescription = data.overview;
           var imdbLink = IMDB_URL + data.imdb;
-          $(document).ready(function(){
-            getDataFromOMDB(data.imdb);
-          });
+          getDataFromOMDB(data.imdb);
+
           var rottenTomatoes = ROTTEN_URL + data.rottentomatoes;
           var commonSenseMedia = data.common_sense_media;
           var metaCritic = data.metacritic;
           var trailerVideo = data.trailers.web[0].embed;
-          var watchLinks = data;
+          var watchLinks = data.purchase_web_sources[0].link;
           var description =
             "<div class='cardDescription hidden'>" +
               "<h1 class='movie-title'>" + data.title + "</h1>" +
               "<h3 class='mpaa-rating'> Rated: " + rated +
-              "<a class='commonsense' target='_blank' title='Common Sense Media' href=" + commonSenseMedia + " ><i class='fa fa-check-circle-o' aria-hidden='true'></i></a><br>" +"</h3>" + meta_rating + " " + imdb_rating +
-              "<h5 class='mpaa-rating'> Genre: " + genre + "</h5>" +
-              "<span>" + movieDescription + "</span><br>" +
+              "<a class='commonsense' target='_blank' title='Common Sense Media' href=" + commonSenseMedia + " ><i class='fa fa-check-circle-o' aria-hidden='true'></i></a><br>" +"</h3>" +
+              "<h5 class='genre'> Genre: " + genre + "</h5>" +
               "<div class='movieLinks'>" +
+              "<a href=" + trailerVideo + " rel='trailervideo' autoplay title='Trailer' data-featherlight='iframe'>"+
+                "<i class='fa fa-youtube-play fa-2x' aria-hidden='true'></i>" +
+                "</a>" +
                 "<a target='_blank' title='IMDB' href=" + imdbLink + "><i class='fa fa-imdb fa-2x' aria-hidden='true'></i></a>" +
                 "<a target='_blank' title='Rotten Tomatoes' href=" + rottenTomatoes +"><i class='fa fa-circle fa-2x' aria-hidden='true'></i></a>" +
-                //<img class='rottentomatoes' src='images/rottentomatoes_icon.png'></a>" +
                 "<a target='_blank'  title='Metacritic' href=" + metaCritic +"><i class='fa fa-meetup fa-2x' aria-hidden='true'></i></a>" +
-                //<img class='metacritic' src='images/metacritic_icon.png'></a>" +
-                // "<a target='_blank' title='Common Sense Media' href=" + commonSenseMedia + " ><i class='fa fa-check-circle-o fa-2x' aria-hidden='true'></i></a><br>" +
-                "<a href=" + trailerVideo + " rel='trailervideo' title='Trailer' target='iframe_a'>"+
-                  "<i class='fa fa-youtube-play fa-3x' aria-hidden='true'></i>" +
-                  //"<iframe height='300px' width='100%' src='' name='iframe_a'></iframe>" +
-                  "</a>" +
               "</div>" +
+              "<span class='movieText'>" + movieDescription + "</span><br>" +
+              "<h5 class='watch'> Rent or Buy </h5>" + "<a target='_blank'  title='Rent/Buy' href=" + watchLinks +"><i class='fa fa-film fa-2x' aria-hidden='true'></i></a>" +
             "</div>";
 
           movieCard = "<div class='movieContainer'><img class='movieCard' src=" + image + ">" + description + "</div>";
@@ -88,35 +77,52 @@ function displaySearchData(data){
     resultElement += '<p> no results <p>';
     ('.js-search-results').append(resultElement);
   }
-};
+  };
 
-$(document).on('click','.movieCard', function(){
-  $(this).addClass("active");
-  $(this).parent().find('.cardDescription').removeClass('hidden');
-  $(this).parent().find('.cardDescription').hide();
-  $('.movieCard').hide();
-  $('.active').show();
-  $(this).animate({
-        left: '30px'
-    }, 1000);
-  $(this).parent().find('.cardDescription').show(1000);
-});
-
-$(document).on('click', '.active', function(){
-  $(this).removeClass('active');
-  $('.movieCard').show();
-  $('.cardDescription').addClass('hidden');
-});
-
-//wait for a submit click
-function watchSubmit(){
-  $('.js-search-form').submit(function(event){
-    $('.js-search-results').empty();
-    event.preventDefault();
-    var query = $(this).find('.js-query').val();
-    getSearchDataFromApi(query, displaySearchData);
+  $(document).on('click','.movieCard', function(){
+    $(this).addClass("active");
+    $(this).parent().find('.cardDescription').removeClass('hidden');
+    $(this).parent().find('.cardDescription').hide();
+    $('.movieCard').hide();
+    $('.active').show();
+    $(this).animate({
+          left: '30px'
+      }, 1000);
+    $(this).parent().find('.cardDescription').show(1000);
   });
-}
+
+  $(document).on('click', '.active', function(){
+    $(this).removeClass('active');
+    $('.movieCard').show();
+    $('.cardDescription').addClass('hidden');
+  });
+
+  //wait for a submit click
+  function watchSubmit(){
+    $('.js-search-form').submit(function(event){
+      $('.js-search-results').empty();
+      event.preventDefault();
+      var query = $(this).find('.js-query').val();
+      getSearchDataFromApi(query, displaySearchData);
+    });
+  }
+
+  //show footer when scrolling to end of page
+  $(window).scroll(function(){
+      if ($(window).scrollTop() == $(document).height() - $(window).height()) {
+          $('footer').show()
+      } else {
+        $('footer').hide();
+      }
+  });
+
+  $(function(){
+    $('.description').hide();
+    $('footer').hide();
+    defaultDisplayData();
+    watchSubmit();
+  });
+});
 
 //load more content once you reach bottom of page
 // $(window).scroll(function() {
@@ -127,9 +133,3 @@ function watchSubmit(){
 //       displaySearchData();
 //    }
 // });
-
-$(function(){
-  $('.description').hide();
-  defaultDisplayData();
-  watchSubmit();
-});
