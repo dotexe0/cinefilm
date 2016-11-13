3192e5795df0f9f5c88b170760e934a12e6a912b
dotexe0 <dv@Daniels-MacBook-Pro.local>
2016-11-13 12:16:21 -0800

    fixed formatting

--- app.js
+++ app.js
@@ -1,33 +1,35 @@
-$(document).ready(function(){
+$(document).ready(function() {
 
-var OMDB_BASE_URL = "https://www.omdbapi.com/?i=";
-var GUIDE_BOX_API_KEY = 'rKqSP9tWheryWwVDGrdBaAZemausGy95';
-var GUIDE_BOX_BASE_URL = 'https://api-public.guidebox.com/v1.43/us/' + GUIDE_BOX_API_KEY + '/search/movie/title/';
-var quota = $.getJSON('https://api-public.guidebox.com/v1.43/us/' + GUIDE_BOX_API_KEY + '/quota', function(data){
-  console.log(data);
-});
-var IMDB_URL = "https://www.imdb.com/title/";
-var ROTTEN_URL = "https://www.rottentomatoes.com/m/";
-//display popular movies by default on main page
-
-function defaultDisplayData(start=0, end=24){
-  var movieSearchURL = 'https://api-public.guidebox.com/v1.43/us/' + GUIDE_BOX_API_KEY + '/movies/all/' + start +'/' + end;
-  $.getJSON(movieSearchURL, displaySearchData)
-}
-
-//search any movie title
-function getSearchDataFromApi(searchTerm, callback) {
-    var query = GUIDE_BOX_BASE_URL + searchTerm + '/fuzzy';
-    $.getJSON(query, callback)
-};
-//grab ratings from OMDB api call
-function getDataFromOMDB(imdbLink) {
-  var search = OMDB_BASE_URL + imdbLink;
-  $.getJSON(search, function(omdb){
-    imdb_rating = omdb.imdbRating;
-    var meta_rating = omdb.Metascore;
+  var OMDB_BASE_URL = "https://www.omdbapi.com/?i=";
+  var GUIDE_BOX_API_KEY = 'rKqSP9tWheryWwVDGrdBaAZemausGy95';
+  var GUIDE_BOX_BASE_URL = 'https://api-public.guidebox.com/v1.43/us/' + GUIDE_BOX_API_KEY + '/search/movie/title/';
+  var quota = $.getJSON('https://api-public.guidebox.com/v1.43/us/' + GUIDE_BOX_API_KEY + '/quota', function(data){
+    console.log(data);
   });
-};
+  var IMDB_URL = "https://www.imdb.com/title/";
+  var ROTTEN_URL = "https://www.rottentomatoes.com/m/";
+
+  //display popular movies by default on main page
+  function defaultDisplayData(start=0, end=24) {
+    var movieSearchURL = 'https://api-public.guidebox.com/v1.43/us/' + GUIDE_BOX_API_KEY + '/movies/all/' + start +'/' + end;
+    $.getJSON(movieSearchURL, displaySearchData)
+  }
+
+  //search any movie title
+  function getSearchDataFromApi(searchTerm, callback) {
+      var query = GUIDE_BOX_BASE_URL + searchTerm + '/fuzzy';
+      $.getJSON(query, callback)
+  };
+
+  //grab ratings from OMDB api call
+  function getDataFromOMDB(imdbLink) {
+    var search = OMDB_BASE_URL + imdbLink;
+    $.getJSON(search, function(omdb){
+      imdb_rating = omdb.imdbRating;
+      var meta_rating = omdb.Metascore;
+    });
+  };
+
   function displaySearchData(data){
   var emptyImage = 'https://static-api.guidebox.com/misc/default_movie_240x342.jpg';
   if (data.results) {
@@ -67,8 +69,7 @@ function getDataFromOMDB(imdbLink) {
               "<span class='movieText'>" + movieDescription + "</span><br>" +
               "<h5 class='watch'> Rent or Buy </h5>" + "<a target='_blank'  title='Rent/Buy' href=" + watchLinks +"><i class='fa fa-film fa-2x' aria-hidden='true'></i></a>" +
             "</div>";
-
-          movieCard = "<div class='movieContainer'><img class='movieCard' src=" + image + ">" + description + "</div>";
+            movieCard = "<div class='movieContainer'><img class='movieCard' src=" + image + ">" + description + "</div>";
           $('.js-search-results').append(movieCard);
         });
       }
@@ -76,7 +77,7 @@ function getDataFromOMDB(imdbLink) {
   } else {
     resultElement += '<p> no results <p>';
     ('.js-search-results').append(resultElement);
-  }
+    }
   };
 
   $(document).on('click','.movieCard', function(){
