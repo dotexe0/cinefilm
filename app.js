cfd936bab6201309ec314802ed5fbf7d853c730b
dotexe0 <dotexe0@gmail.com>
2017-08-15 16:34:41 -0700

    updated css

--- app.js
+++ app.js
@@ -2,15 +2,9 @@ require("babel-core/register");
 require("babel-polyfill");
 
 $(document).ready(function() {
-
-  // let OMDB_BASE_URL = "https://www.omdbapi.com/?t=";
-  let THE_MOVIE_DB_API_KEY = '074c1de1f173b40ae75cddd1cebe2527';
-  // let THE_MOVIE_DB_BASE_URL = `https://api.themoviedb.org/3/search/movie?api_key=${THE_MOVIE_DB_API_KEY}&language=en-US&query=${query}&page=1&include_adult=false`;  // let GUIDE_BOX_API_KEY = 'rKqSP9tWheryWwVDGrdBaAZemausGy95';
-  // let GUIDE_BOX_BASE_URL = 'https://api-public.guidebox.com/v2/search?api_key=' + GUIDE_BOX_API_KEY + '&type=movie&limit=10&field=title&precision=fuzzy&query=';
-  // let quota = $.getJSON('https://api-public.guidebox.com/v1.43/us/' + GUIDE_BOX_API_KEY + '/quota', function(data){
-  // });
-  let IMDB_URL = "https://www.imdb.com/title/";
-  let ROTTEN_URL = "https://www.rottentomatoes.com/m/";
+  const THE_MOVIE_DB_API_KEY = '074c1de1f173b40ae75cddd1cebe2527';
+  const IMDB_URL = "https://www.imdb.com/title/";
+  const ROTTEN_URL = "https://www.rottentomatoes.com/m/";
 
   //display popular movies by default on main page
   function defaultDisplayData() {
@@ -44,27 +38,18 @@ $(document).ready(function() {
       
     ).then(function() {
       if(movieData) {
-        console.log('first api called');        
         var title = movieData.original_title;
-        var image = `https://image.tmdb.org/t/p/w185${movieData.poster_path}`;
-        
+        var image = movieData.poster_path ? `https://image.tmdb.org/t/p/w185${movieData.poster_path}`: 'https://static-api.guidebox.com/misc/default_movie_240x342.jpg' 
         var rated = movieData.vote_average;
         var genre = movieData.genres[0] ? movieData.genres[0].name : 'N/A';
         var imdbLink = `${IMDB_URL}${movieData.imdb_id}`;
         var movieDescription = movieData.overview;
-        // var trailerVideo = getTrailerVideo(TRAILER_API_ENDPOINT);
-                      // , , , imdbLink, rottenTomatoes, metaCritic, description, watchLinks, 
         var movieCard = '';
-        if (!movieData.homepage) {
-          var watchLinks = '#';
-        } else {
-          var watchLinks = movieData.homepage;
-        }
+        var homepage = movieData.homepage ? movieData.homepage : '#';
       }
       else {
         //errror on first api call
-        console.log('API 1 didnt worked');        
-        
+        console.log('API 1 didnt worked');
       }
       if (trailerData) {
         console.log('API 2 worked');        
@@ -73,19 +58,16 @@ $(document).ready(function() {
         var description =
         "<div class='cardDescription hidden'>" +
           "<h1 class='movie-title'>" + movieData.original_title + "</h1>" +
-          "<h3 class='mpaa-rating'> Avg. Rating: " + rated +
-          // "<a class='commonsense' target='_blank' title='Common Sense Media' href=" + commonSenseMedia + " ><i class='fa fa-check-circle-o' aria-hidden='true'></i></a><br>" +"</h3>" +
+          "<h3 class='mpaa-rating'> Average Rating: " + rated +
           "<h5 class='genre'> Genre: " + genre + "</h5>" +
           "<div class='movieLinks'>" +
           "<a href=" + trailerVideo + " rel='trailervideo' autoplay title='Trailer' data-featherlight='iframe'>"+
-            "<i class='fa fa-youtube-play fa-2x' aria-hidden='true'></i>" +
+            "<i class='fa fa-youtube-play fa-3x' aria-hidden='true'></i>" +
             "</a>" +
-            "<a target='_blank' title='IMDB' href=" + imdbLink + "><i class='fa fa-imdb fa-2x' aria-hidden='true'></i></a>" +
-            // "<a target='_blank' title='Rotten Tomatoes' href=" + rottenTomatoes +"><i class='fa fa-circle fa-2x' aria-hidden='true'></i></a>" +
-            // "<a target='_blank'  title='Metacritic' href=" + metaCritic +"><i class='fa fa-meetup fa-2x' aria-hidden='true'></i></a>" +
+            "<a target='_blank' title='IMDB' href=" + imdbLink + "><i class='fa fa-imdb fa-3x' aria-hidden='true'></i></a>" +
           "</div>" +
           "<span class='movieText'>" + movieDescription + "</span><br>" +
-          "<h5 class='watch'> Rent or Buy </h5>" + "<a target='_blank'  title='Rent/Buy' href=" + watchLinks +"><i class='fa fa-film fa-2x' aria-hidden='true'></i></a>" +
+          "<h5 class='watch'> Movie Homepage </h5>" + "<a target='_blank'  title='Homepage' href=" + homepage +"><i class='fa fa-film fa-2x' aria-hidden='true'></i></a>" +
         "</div>";
         movieCard = "<div class='movieContainer'><img class='movieCard' src=" + image + ">" + description + "</div>";
         $('.js-search-results').append(movieCard);  
@@ -96,26 +78,6 @@ $(document).ready(function() {
       }
       });     
   };
-
-
-  //get trailers
-  function getTrailerVideo(url) {
-    $.getJSON(url, function(data) {
-      // console.log('video url: ', data)
-      if (data.results.length === 0 ) {
-        let id = 'dQw4w9WgXcQ';
-        let trailerVideo = `https://www.youtube.com/embed/${id}`;
-        return trailerVideo;
-      } else {
-        let id = data.results[0].key;
-        let trailerVideo = `https://www.youtube.com/embed/${id}`;
-        return trailerVideo;
-      }
-
-    })
-    .done( data => console.log('data'))
-    .fail(function() { let trailerVideo = 'https://www.youtube.com/embed/dQw4w9WgXcQ' });
-  }
 
   function displaySearchData(data){
     console.log('initial data', data);
